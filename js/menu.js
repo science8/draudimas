0c111ac67025c7439173f9b5b8ac78359d445ceb
science8 <39078067+science8@users.noreply.github.com>
2019-05-15 16:51:58 +0300

    done1

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+//start1
+ $('.topflex').on('mouseover', event => {
+    $(event.currentTarget).addClass('color1')
+     $(event.currentTarget).removeClass('topflex')
+  })
+ $('.topflex').on('mouseleave', event => {
+    $(event.currentTarget).removeClass('color1')
+    $(event.currentTarget).addClass('topflex')
+  })
+//end_1
+
+//start_2
+
+
+
+//end_2
+
+
+			});
+
